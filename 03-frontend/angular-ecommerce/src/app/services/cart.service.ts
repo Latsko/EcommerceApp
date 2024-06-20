e06d84a43946a24f472c956e8d1a45536b1fa7a4
Stanislav Latsko <01latsko10@gmail.com>
2024-06-20 03:08:48 +0200

    commit - implemented the cart service class

--- 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
+++ 03-frontend/angular-ecommerce/src/app/services/cart.service.ts
@@ -13,4 +13,65 @@ export class CartService {
   totalQuantity: Subject<number> = new Subject<number>();
 
   constructor() { }
+
+  addToCart(theCartItem: CartItem) {
+    // check if we already have the item in our cart
+    let alreadyExistsInCart: boolean = false;
+    let existingCartItem: CartItem | undefined = undefined;
+
+    if (this.cartItems.length > 0) {
+      // find item in the cart based on item id
+
+      for (let tempCartItem of this.cartItems) {
+        if (tempCartItem.id === theCartItem.id) {
+          existingCartItem = tempCartItem;
+          break;
+        }
+      }
+
+      //check if we found it
+      alreadyExistsInCart = (existingCartItem != undefined);
+    }
+
+    if(alreadyExistsInCart) {
+      // increment the quantity
+      existingCartItem!.quantity++;
+    } else {
+      // just add the item to the array
+      this.cartItems.push(theCartItem);
+
+      //compute cart total price and total quantity
+      this.computeCartTotals();
+    }
+  }
+
+  computeCartTotals() {
+    let totalPriceValue: number = 0;
+    let totalQunatityValue: number = 0;
+
+    for (let currentCartItem of this.cartItems) {
+      totalPriceValue += currentCartItem.quantity * currentCartItem.unitePrice;
+      totalQunatityValue += currentCartItem.quantity;
+    }
+
+    // publish the new values ... all subscribers will receive the new data
+    this.totalPrice.next(totalPriceValue);
+    this.totalQuantity.next(totalQunatityValue);
+
+    // log cart data just for debugging purposes
+    this.logCartData(totalPriceValue, totalQunatityValue);
+  }
+
+  logCartData(totalPriceValue: number, totalQunatityValue: number) {
+    
+    console.log('Contents of the cart');
+    for (let tempCartItem of this.cartItems) {
+      const subTotalPrice = tempCartItem.unitePrice * tempCartItem.unitePrice;
+      console.log(`name: ${tempCartItem.name}, quantity=${tempCartItem.quantity},
+         unitPrice=${tempCartItem.unitePrice}, subTotalPrice=${subTotalPrice}`);
+    }
+
+    console.log(`totalPrice: ${totalPriceValue.toFixed(2)}, totalQuantity: ${totalQunatityValue}`);
+    console.log('-----');
+  }
 }
