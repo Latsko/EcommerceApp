cc4df5e8ea5023301a61c289cf0b37542399d495
Stanislav Latsko <01latsko10@gmail.com>
2024-06-21 22:38:44 +0200

    commit - (backend) added country and state entities and repos, configured MyDataRestConfig

---
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/dao/CountryRepository.java
@@ -0,0 +1,12 @@
+package com.latsko.ecommerce.dao;
+
+import com.latsko.ecommerce.entity.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("http://localhosst:4200")
+@RepositoryRestResource(collectionResourceRel = "countries", path = "countries")
+public interface CountryRepository extends JpaRepository<Country, Integer> {
+}
--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -91,4 +91,26 @@ export class CheckoutComponent implements OnInit {
     }
   }
 
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup!.value.expirationYear);
+
+    let startMonth: number;
+
+    if(currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.eCommerceFormService.getCreditCardMonths(startMonth).subscribe(
+      data => {
+        console.log("Retrieved credit card months: " + JSON.stringify(data));
+        this.creditCardMonths = data;
+      }
+    )
+  }
+
 }
