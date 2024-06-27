0f8295e0a0051cb52457d111973ee2d57e48a266
Stanislav Latsko <01latsko10@gmail.com>
2024-06-27 02:25:32 +0200

    commit - (backend) added Address, OrderItem and Order entities

--- 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/entity/Customer.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/entity/Customer.java
@@ -1,7 +1,6 @@
 package com.latsko.ecommerce.entity;
 
 import jakarta.persistence.*;
-import lombok.Generated;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -13,11 +12,15 @@ public class Customer {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "first_name")
     private String firstName;
 
+    @Column(name = "last_name")
     private String lastName;
 
+    @Column(name = "email")
     private String email;
 }
