ad0bd16b1928f979f116df7924851d704d6996b2
Stanislav Latsko <01latsko10@gmail.com>
2024-07-09 00:41:52 +0200

    commit - (backend) configured cors for controller in new separate MyAppConfig class, made only annotated rest repositories visible from browsers

--- 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/controller/CheckoutController.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/controller/CheckoutController.java
@@ -5,7 +5,6 @@ import com.latsko.ecommerce.dto.PurchaseResponse;
 import com.latsko.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
