d177641cbcbeeee4b95f4eccd4cc0f13af3fa9ba
Stanislav Latsko <01latsko10@gmail.com>
2024-07-10 23:12:17 +0200

    commit - (backend) created configuration class for SecurityFilterChain in order to secure url with orders

--- 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
+++ 03-frontend/angular-ecommerce/src/app/components/checkout/checkout.component.ts
@@ -30,7 +30,8 @@ export class CheckoutComponent implements OnInit {
 
  shippingAddressStates: State[] = [];
  billingAddressStates: State[] = [];
- 
+
+ storage: Storage = sessionStorage;
 
  constructor(private formBuilder: FormBuilder,
              private eCommerceFormService: ECommerceFormService,
@@ -42,6 +43,9 @@ export class CheckoutComponent implements OnInit {
 
     this.reviewCartDetails();
 
+    // read the user's emial address from browser storage
+    const theEmail = JSON.parse(this.storage.getItem('userEmail')!);
+
     this.checkoutFormGroup = this.formBuilder.group({
       customer: this.formBuilder.group({
         firstName: new FormControl('',
@@ -52,7 +56,7 @@ export class CheckoutComponent implements OnInit {
                                   [Validators.required,
                                   Validators.minLength(2),
                                   ECommerceValidators.notOnlyWhitespace]),
-        email: new FormControl('',
+        email: new FormControl(theEmail,
                                [Validators.required,
                                Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]
         )
@@ -130,6 +134,7 @@ export class CheckoutComponent implements OnInit {
     )
     
   }
+
   reviewCartDetails() {
     
     // subscribe to cartService.(totalQuantity and totalPrice)
