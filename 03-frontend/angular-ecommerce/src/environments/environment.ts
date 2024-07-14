00cfb8dde45ff3f02cb312ef5a2df97333177c10
Stanislav Latsko <01latsko10@gmail.com>
2024-07-14 16:21:11 +0200

    commit - added package for stripe, created payment info class and typings.d.ts

--- 03-frontend/angular-ecommerce/src/environments/environment.ts
+++ 03-frontend/angular-ecommerce/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    latskoShopApiUrl: "https://localhost:8443/api"
+    latskoShopApiUrl: "https://localhost:8443/api",
+    stripePublishableKey: "pk_test_51Pbo91HXVxiJ3q7mPnh8ajg3WI6yRh1c2oklSHrFiztOE2e6ojqtJYPYWTh4gltHMfKHkNPEox0HMeJ48Ibglmtn008TVUVbxL"
 };
