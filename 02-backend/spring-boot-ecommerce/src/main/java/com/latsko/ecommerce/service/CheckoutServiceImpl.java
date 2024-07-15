49e93f95c06a4b89f4073611822d2be2611d2ae0
Stanislav Latsko <01latsko10@gmail.com>
2024-07-15 04:23:11 +0200

    commit - added new stripe fields for payment confirmation

--- 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/service/CheckoutServiceImpl.java
+++ 02-backend/spring-boot-ecommerce/src/main/java/com/latsko/ecommerce/service/CheckoutServiceImpl.java
@@ -75,6 +75,7 @@ public class CheckoutServiceImpl implements CheckoutService {
         params.put("amount", paymentInfo.getAmount());
         params.put("currency", paymentInfo.getCurrency());
         params.put("payment_method_types", paymentMethodTypes);
+        params.put("description", "LatskoECommerce purchase");
 
         return PaymentIntent.create(params);
     }
